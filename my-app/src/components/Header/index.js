fbe4fd5c1a076a3b56d8ab0a55ddfb5ca52a0973
hiep.tran2 <hiep.tran2@asiantech.vn>
2021-08-09 17:06:27 +0700

    day8 Redux - Redux toolkit

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import logoHeader from './../../assets/img/logo.svg';
-import { FaCartArrowDown, FaUserAlt } from "react-icons/fa";
+import { FaCartArrowDown, FaUserAlt, FaHeart } from "react-icons/fa";
 import { NavLink, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  // const count = useSelector(state => state.counter.value)
+  const favs = useSelector(state => state.fav.value);
+  console.log(favs);
+
   return (
     <>
     <header className="header-page">
       <div className="container">
         <div className="header-box">
           <div className="header-logo">
-            <Link to="/">
+            <NavLink to="/">
               <img className="logo-header" src={logoHeader} alt="logo" />
-            </Link>
+            </NavLink>
           </div>
           <nav className="navbar">
             <ul className="nav-list">
@@ -28,6 +33,12 @@ const Header = () => {
             </ul>
           </nav>
           <ul className="social-list">
+            <li><NavLink to="/account"> 
+              <span className="wrap-fav-couter">
+                <FaHeart />
+                  {!!favs.length && <span>{favs.length}</span>}
+              </span>
+            </NavLink></li>
             <li><NavLink to="/account"><FaUserAlt /></NavLink></li>
             <li><NavLink to="/"><FaCartArrowDown /></NavLink></li>
           </ul>
--- my-app/src/pages/Features/ProductDetail/index.js
+++ my-app/src/pages/Features/ProductDetail/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import logo from '../../../assets/img/product.jpg';
 import { useParams } from 'react-router-dom';
+import { increment, decrement } from '../../../store/couterSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { FaHeart } from 'react-icons/fa';
 
 const ProductDetail = () => {
   const { id } = useParams();
-  // console.log(id);
+  // const count = useSelector(state => state.counter.value)
+  // const dispatch = useDispatch();
 
   return (
     <section className="section-product-detail">
       <div className="container">
         <div className="product-detail-wrap">
-          <h3 className="product-name">Product Detail</h3>
+          <h3 className="product-name">Product Detail {id}</h3>
           <div className="product-card">
-            <img className="product-image" src={logo} alt="logo" />
+            <div className="product-image">
+              <img  src={logo} alt="logo" />
+              {/* <span className="product-fav"><FaHeart /></span> */}
+            </div>
             <div>
               <p className="product-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna
